39d10b952bf892dd69ff4680565c6661f90b1325
Anthony Sotomayor <anthonysv@gmail.com>
2022-12-03 17:20:31 -0500

    RETO 05

--- RETO-05.js
+++ RETO-05.js
@@ -1,12 +1,13 @@
 'use strict'
 /* Con la emoción, ya estamos empezando a contar los días del calendario hasta el 25 de diciembre 📆.
-
 Para ayudar a esto, vamos a crear una función que pasándole una instancia de Date nos diga el número de días que faltan.
-
 El resultado tiene que ser un número entero y, como ves, aunque falte un segundo hasta el siguiente día, se entiende que todavía falta un día.
-
 ¡Pero ojo! También hay que indicar si la fecha es del mismo día (devolveríamos 0) o si es una fecha futura (devolveríamos el número de días en negativo -): */
 function daysToXmas(date) {
-  // ¡Y no olvides compartir tu solución en redes!
-  return 0
+  const navidad = new Date('Dec 25, 2021')
+  //Dias en el año
+  const unDia = 1000 * 60 * 60 * 24
+  const diferencia = navidad - date
+
+  return Math.ceil(diferencia / unDia)
 }
