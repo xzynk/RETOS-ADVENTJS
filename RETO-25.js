3385034b4ba902ef05a44ab26399fbd0b18682ab
Anthony Sotomayor <anthonysv@gmail.com>
2022-12-16 15:30:29 -0500

    RETO 25

--- RETO-25.js
+++ RETO-25.js
@@ -42,7 +42,43 @@ canMouseEat('left',  room2)   // false
 y que cada una puede tener dimensiones diferentes! */
 
 function canMouseEat(direction, game) {
-  return false
+  const MOUSE = 'm'
+  const FOOD = '*'
+
+  //Buscamos al raton para saber en que array y en que indice se encuentra
+  const mouseIndex = (gameArray) => {
+    const index = gameArray.findIndex((element) => element.includes(MOUSE))
+    if (index === -1) return false
+    return { x: index, y: gameArray[index].indexOf(MOUSE) }
+  }
+
+  const mouseIndexPosition = mouseIndex(game)
+  const checkFood = (mouseObj) => {
+    //Modificamos el array segun la direccion
+    switch (direction) {
+      case 'up':
+        mouseObj.x -= 1
+        break
+      case 'down':
+        mouseObj.x += 1
+        break
+      case 'left':
+        mouseObj.y -= 1
+        break
+      default:
+        mouseObj.y += 1
+        break
+    }
+
+    // Estamos intentando buscar la comida en una posicion inexistente?
+    if (mouseObj.x < 0 || mouseObj.y > game.length - 1) return false
+    // Buscamos la comida si encontramos cualquier otra cosa entonces FALSE
+    if (game[mouseObj.x][mouseObj.y] !== FOOD) return false
+    // Si no retornamos TRUE
+    return true
+  }
+
+  return checkFood(mouseIndexPosition)
 }
 
 const room = [
@@ -52,3 +88,18 @@ const room = [
 ]
 
 canMouseEat('up', room) // false
+canMouseEat('down', room) // true
+canMouseEat('right', room) // false
+canMouseEat('left', room) // false
+
+const room2 = [
+  ['*', ' ', ' ', ' '],
+  [' ', 'm', '*', ' '],
+  [' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', '*'],
+]
+
+canMouseEat('up', room2) // false
+canMouseEat('down', room2) // false
+canMouseEat('right', room2) // true
+canMouseEat('left', room2) // false
